63f0342ae42bb9a527ee7007a2254904aa076eea
Yifan Tang <yifant@g.ucla.edu>
2024-03-03 12:44:58 -0800

    Added Landing, Query, and Results pages

---
+++ frontend/src/pages/LandingPage.js
@@ -0,0 +1,3 @@
+export default function LandingPage() {
+  return <div>This is the Landing Page.</div>;
+}
---
+++ frontend/src/pages/ResultsPage.js
@@ -0,0 +1,3 @@
+export default function ResultsPage() {
+  return <div>This is the Results Page.</div>;
+}
